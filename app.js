2ddfdfffef85845aa0ef2963a23e5ee9a927239c
Francis Chung <francis.c.chung@gmail.com>
2025-10-15 12:07:51 +1100

    hx-trigger example with multiple events

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => {
           <p>HTMX is a JavaScript library that you use without writing JavaScript code.</p>
           <button 
             hx-get="/info"
-            hx-trigger="mouseenter[ctrlKey]" 
+            hx-trigger="mouseenter[ctrlKey],click" 
             hx-target="main" 
             hx-swap="beforeend" >Learn More</button>
         </main>
