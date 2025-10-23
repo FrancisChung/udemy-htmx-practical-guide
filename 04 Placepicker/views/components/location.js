1ed9b60214e852074ffc38e1f8c83679c9bf86a9
Francis Chung <francis.c.chung@gmail.com>
2025-10-23 16:12:57 +1100

    add hx-target and hx-swap entries to button

--- 04 Placepicker/views/components/location.js
+++ 04 Placepicker/views/components/location.js
@@ -3,7 +3,9 @@ export default function renderLocation(location) {
     <li class="location-item">
       <button 
       hx-post="/places" 
-      hx-vals='{"locationId": "${location.id}"'>
+      hx-vals='{"locationId": "${location.id}"}'
+      hx-target="#interesting-locations"
+      hx-swap="beforeend">
         <img src="${`/images/${location.image.src}`}" alt="${location.image.alt}" />
         <h3>${location.title}</h3>
       </button>
