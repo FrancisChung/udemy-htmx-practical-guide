1e8e4685a328bcd66ebe0bd70cc96158e6aee855
Francis Chung <francis.c.chung@gmail.com>
2025-10-24 10:57:07 +1100

    showConfirmationModal implementation

--- 04 Placepicker/public/main.js
+++ 04 Placepicker/public/main.js
@@ -1,5 +1,25 @@
-function showConfirmationModal() {
-    console.log("Showing Modal...");
+function showConfirmationModal(event) {
+    event.preventDefault();
+    console.log("Event", event);
+    const confirmationModal = `
+      <dialog class="modal">
+        <div id="confirmation">
+          <h2>Are you sure?</h2>
+          <p>Do you really want to ACTION this place?</p>
+          <div id="confirmation-actions">
+            <button id="action-no" className="button-text">
+              No
+            </button>
+            <button id="action-yes" className="button">
+              Yes
+            </button>
+          </div>
+        </div>
+      </dialog>
+    `;
+    document.body.insertAdjacentHTML('beforeend', confirmationModal);
+    const dialog = document.querySelector('dialog');
+    dialog.showModal();
 }
 
 document.addEventListener('htmx:beforeRequest', showConfirmationModal);
