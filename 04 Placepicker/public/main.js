c63c099b19bb191668a632c76c217d51101aac34
Francis Chung <francis.c.chung@gmail.com>
2025-10-24 13:08:25 +1100

    Implementing dataset.action

--- 04 Placepicker/public/main.js
+++ 04 Placepicker/public/main.js
@@ -1,24 +1,27 @@
 function showConfirmationModal(event) {
     event.preventDefault();
     console.log("Event", event);
+    const action = event.detail.elt.dataset.action;
     const confirmationModal = `
       <dialog class="modal">
         <div id="confirmation">
           <h2>Are you sure?</h2>
-          <p>Do you really want to ACTION this place?</p>
+          <p>Do you really want to ${action} this place?</p>
           <div id="confirmation-actions">
             <button id="action-no" className="button-text">
               No
             </button>
             <button id="action-yes" className="button">
               Yes
-            </button>
+                </button>
           </div>
         </div>
       </dialog>
     `;
     document.body.insertAdjacentHTML('beforeend', confirmationModal);
     const dialog = document.querySelector('dialog');
+
+
     dialog.showModal();
 }
 
