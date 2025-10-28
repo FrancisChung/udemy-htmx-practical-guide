8d766cd2e0aa4d4f8273ea65ab8245f62f995e3b
Francis Chung <francis.c.chung@gmail.com>
2025-10-28 18:32:36 +1100

    HX-Retarget, HX-Reswap example

--- 05 Requests Responses/app.js
+++ 05 Requests Responses/app.js
@@ -111,6 +111,8 @@ app.post('/login', (req, res) => {
   }
 
   if (Math.random() > 0.5) {
+      res.setHeader('HX-Retarget','.control');
+      res.setHeader('HX-Reswap','beforebegin');
       return res.send(`
         <p class="error">An error has occurred. Please try again</p>
     `)
